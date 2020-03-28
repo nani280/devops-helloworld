d5575754922b32907f58c5fa42b89e6b6d5ba8c1
nani280 <62784003+nani280@users.noreply.github.com>
2020-03-28 10:29:10 +0530

    second commit

--- sample_code.sh
+++ sample_code.sh
@@ -2,6 +2,7 @@
 echo "This is an Samples Script from GitHub"
 echo"this is nani"
 echo "This is test from Linux Server"
+ech0" this is hari"
 echo "This is an example of a script"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
